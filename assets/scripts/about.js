7dfd10dfa55d20a9bde3ef129990485586c31cd4
Guillaume <guillaume.andry@hotmail.fr>
2024-06-12 16:37:41 +0200

    mise à jour about

--- assets/scripts/about.js
+++ assets/scripts/about.js
@@ -3,15 +3,30 @@ function functionPortraitName(element) {
     const hidden=element.querySelector(".hidden-txt");
     const portrait=element.querySelector(".portrait-text");
     const click=element.dataset.state==='1'?true:false;
-    if (!click && window.matchMedia("(max-width: 900px)").matches) {
+    if (!click && window.matchMedia("(max-width: 1000px)").matches) {
         name.style = "position:relative; top:-329px; border-radius: 15px; padding-bottom: 239px; margin-bottom: -32px;";
         element.dataset.state="1";
         portrait.style = "margin-top: -299px;";
         hidden.style = "visibility: visible; position: relative; top: -470px; left: 30px;";
-    } else if (window.matchMedia("(max-width: 900px)").matches) {
+    } else if (window.matchMedia("(max-width: 1000px)").matches) {
         name.style = "";
         portrait.style = "";
         element.dataset.state="0";
         hidden.style = "visibility: hidden";
     }
 }
+
+// function functionBackground(element) {
+//     document.getElementById(element).animate(
+//         [
+//           // étapes/keyframes
+//           { transform: "translateY(0px)" },
+//           { transform: "translateY(-300px)" },
+//         ],
+//         {
+//           // temporisation
+//           duration: 1000,
+//           iterations: Infinity,
+//         },
+//       );
+// }
