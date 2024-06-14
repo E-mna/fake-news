73b9814d92413fbe2a687fb457471b2f812f76b9
styvens-w <styvens.wentz@gmail.com>
2024-06-14 09:40:16 +0200

    about : modification de l'animation du banner

--- assets/scripts/about.js
+++ assets/scripts/about.js
@@ -1,35 +1,38 @@
 function functionPortraitName(element) {
-    const name = element.querySelector('.portrait-name');
-    const hidden = element.querySelector(".hidden-txt");
-    const portrait = element.querySelector(".portrait-text");
-    const click = element.dataset.state === '1' ? true : false;
-    if (!click && window.matchMedia("(max-width: 1000px)").matches) {
-        name.style = "position:relative; top:-329px; border-radius: 15px; padding-bottom: 239px; margin-bottom: -32px;";
-        element.dataset.state = "1";
-        portrait.style = "margin-top: -299px;";
-        hidden.style = "visibility: visible; position: relative; top: -470px; left: 30px;";
-    } else if (window.matchMedia("(max-width: 1000px)").matches) {
-        name.style = "";
-        portrait.style = "";
-        element.dataset.state = "0";
-        hidden.style = "visibility: hidden";
-    }
+  const name = element.querySelector(".portrait-name");
+  const hidden = element.querySelector(".hidden-txt");
+  const portrait = element.querySelector(".portrait-text");
+  const click = element.dataset.state === "1";
+  if (!click && window.matchMedia("(max-width: 1000px)").matches) {
+    name.style =
+      "position:relative; top:-329px; border-radius: 15px; padding-bottom: 239px; margin-bottom: -32px;";
+    element.dataset.state = "1";
+    portrait.style = "margin-top: -299px;";
+    hidden.style =
+      "visibility: visible; position: relative; top: -470px; left: 30px;";
+  } else if (window.matchMedia("(max-width: 1000px)").matches) {
+    name.style = "";
+    portrait.style = "";
+    element.dataset.state = "0";
+    hidden.style = "visibility: hidden";
+  }
 }
 
 // document.getComputedStyle("about-banner", ":background-image").animate(
-    //     document.getElementById("about-banner").animate(
-function functionBg() {
-    document.getElementById("about-banner").animate(
-        [
-            // étapes/keyframes
-            { transform: "translateX(0px)" },
-            { transform: "translateX(50vw)" },
-            { transform: "rotate(90deg)" },
-        ],
-        {
-            // temporisation
-            duration: 1000,
-            iterations: 1,
-        },
-    );
+//     document.getElementById("about-banner").animate(
+function functionBg(element) {
+  element.animate(
+    [
+      // étapes/keyframes
+      { transform: "translateX(0px)" },
+      { transform: "translateX(calc(50vw - 280px))" },
+      { transform: "rotateY(90deg)" },
+      { transform: "rotateY(0deg)" },
+    ],
+    {
+      // temporisation
+      duration: 1000,
+      iterations: 1,
+    }
+  );
 }
--- pages/about.php
+++ pages/about.php
@@ -1,16 +1,77 @@
 <main class="main">
-    <div class="bgRed"><div class="about-banner" onclick="functionBg(this)" id="about-banner"></div></div>            
-    <h1>Vous voulez en savoir plus sur notre canard ?</h1>
-    <h2 class="intro team">Notre équipe de rédaction</h2>
-    <div class="profilpic">
-        <div onclick="functionPortraitName(this)"><img src="../assets/img/Marc.webp" class="portrait" alt="Portrait of Marc"><p class="portrait-name" id="portrait-name1">Marc</p><p class="hidden-txt" id="hidden-txt1">Age : 60 <br> Profession : Rédacteur en chef</p><p class="portrait-text" id="portrait-text1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum maxime dolorem est quis. Amet maiores odit, quia facilis, minus numquam commodi exercitationem sit aspernatur animi libero nulla necessitatibus. Quod, dolorem?</p></div>
-        <div class="switch-back" onclick="functionPortraitName(this)"><img src="../assets/img/Isa.webp" class="portrait" alt="Portrait of Isa"><p class="portrait-name" id="portrait-name2">Isa</p><p class="hidden-txt" id="hidden-txt2">Age : 30 <br> Profession : Journaliste</p><p class="portrait-text" id="portrait-text2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum qui eligendi illum fugiat autem! Laborum molestias culpa, eum ad quo eius fuga, impedit explicabo vel sequi obcaecati, dolorem eligendi eos?</p></div>
-        <div onclick="functionPortraitName(this)"><img src="../assets/img/Baptiste.webp" alt="Portrait of Baptiste" class="portrait"><p class="portrait-name" id="portrait-name3">Baptiste</p><p class="hidden-txt" id="hidden-txt3">Age : 25 <br> Profession : Journaliste</p><p class="portrait-text" id="portrait-text3">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque architecto provident omnis iste tempora deleniti autem fugiat, eius, porro maiores rem corrupti cum iusto veniam ullam dolore ea odit ab.</p></div>
-    </div>
-    <h2 class="intro espace">Nos opens spaces</h2>
-    <div class="localisation">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1314240.2926226875!2d1.499100864154607!3d49.957436561818994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e7f8c099b9bdb9%3A0x90b400cd653852e2!2sHauts-de-France!5e0!3m2!1sfr!2sfr!4v1717502627025!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="maps"></iframe>
-        <p class="localisation-txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore quo blanditiis quod cumque, architecto impedit tempora consectetur. Et voluptates aliquam tempore, neque qui consequatur distinctio delectus sequi ipsa inventore. Quod. Lore Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit minima neque ducimus eius illo cumque vitae quisquam error harum, consequuntur </p>
+    <div class="bgRed">
+        <div class="about-banner" onclick="functionBg(this)" id="about-banner"></div>
     </div>
 
+    <h1>Vous voulez en savoir plus sur notre canard ?</h1>
+    <h2 class="intro team">Notre équipe de rédaction</h2>
+
+    <div class="profilpic">
+        <div onclick="functionPortraitName(this)">
+            <img src="../assets/img/Marc.webp" class="portrait" alt="Portrait of Marc">
+
+            <p class="portrait-name" id="portrait-name1">
+                Marc
+            </p>
+
+            <p class="hidden-txt" id="hidden-txt1">
+                Age : 60 <br>
+                Profession : Rédacteur en chef
+            </p>
+
+            <p class="portrait-text" id="portrait-text1">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum maxime dolorem est quis. Amet maiores
+                odit, quia facilis, minus numquam commodi exercitationem sit aspernatur animi libero nulla
+                necessitatibus. Quod, dolorem?
+            </p>
+        </div>
+
+        <div class="switch-back" onclick="functionPortraitName(this)">
+            <img src="../assets/img/Isa.webp" class="portrait" alt="Portrait of Isa">
+
+            <p class="portrait-name" id="portrait-name2">
+                Isa
+            </p>
+
+            <p class="hidden-txt" id="hidden-txt2">Age : 30
+                <br> Profession : Journaliste
+            </p>
+
+            <p class="portrait-text" id="portrait-text2">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum qui eligendi illum fugiat autem! Laborum
+                molestias culpa, eum ad quo eius fuga, impedit explicabo vel sequi obcaecati, dolorem eligendi eos?
+            </p>
+        </div>
+
+        <div onclick="functionPortraitName(this)">
+            <img src="../assets/img/Baptiste.webp" alt="Portrait of Baptiste" class="portrait">
+
+            <p class="portrait-name" id="portrait-name3">
+                Baptiste
+            </p>
+
+            <p class="hidden-txt" id="hidden-txt3">
+                Age : 25<br>
+                Profession : Journaliste
+            </p>
+
+            <p class="portrait-text" id="portrait-text3">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque architecto provident omnis iste tempora
+                deleniti autem fugiat, eius, porro maiores rem corrupti cum iusto veniam ullam dolore ea odit ab.
+            </p>
+        </div>
+    </div>
+
+    <h2 class="intro espace">Nos opens spaces</h2>
+
+    <div class="localisation">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1314240.2926226875!2d1.499100864154607!3d49.957436561818994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e7f8c099b9bdb9%3A0x90b400cd653852e2!2sHauts-de-France!5e0!3m2!1sfr!2sfr!4v1717502627025!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="maps"></iframe>
+
+        <p class="localisation-txt">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore quo blanditiis quod cumque, architecto
+            impedit tempora consectetur. Et voluptates aliquam tempore, neque qui consequatur distinctio delectus sequi
+            ipsa inventore. Quod. Lore Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit minima neque
+            ducimus eius illo cumque vitae quisquam error harum, consequuntur
+        </p>
+    </div>
 </main>
